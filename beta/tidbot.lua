862e5ff239476512e97da3aff053f11c629462e4
zep715 <zep715@gmail.com>
2017-03-20 15:23:40 +0100

    Update tidbot.lua

--- beta/tidbot.lua
+++ beta/tidbot.lua
@@ -8,23 +8,23 @@ end
 local version = memory.readword(0x14e)
 if version == 0xc1a2 or version == 0x36dc or version == 0xd5dd or version == 0x299c then
 	print("RBGY JPN game detected")
-  a_tid = 0xd2d8
+  	a_tid = 0xd2d8
 elseif version == 0xe691 or version == 0xa9d then
 	print("RB USA detected")
-  a_tid = 0xd359
+	a_tid = 0xd359
 elseif version == 0x7c04 then
 	print("Y USA detected")
-  a_tid = 0xd358
+  	a_tid = 0xd358
 elseif version == 0xd289 or version == 0x9c5e or version == 0xdc5c or version == 0xbc2e or version == 0x4a38 or version == 0xd714 or version == 0xfc7a or version == 0xa456 then
 	print("RB EUR detected")
-  a_tid = 0xd35e
+  	a_tid = 0xd35e
 elseif version == 0x8f4e or version == 0xfb66 or version == 0x3756 or version == 0xc1b7 then
 	print("Y EUR detected")
-  a_tid = 0xd35d
+  	a_tid = 0xd35d
 else
 	print(string.format("unknown version, code: %4x", version))
-  print("script stopped")
-  return
+ 	print("script stopped")
+  	return
 end
 
 desired_tid = reverseword(desired_tid)
