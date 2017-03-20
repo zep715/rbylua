d9c6d12e7e7af870fed6176ce1108e4a0feee9d1
zep715 <zep715@gmail.com>
2017-03-21 00:14:38 +0100

    Update party_checker.lua

--- beta/party_checker.lua
+++ beta/party_checker.lua
@@ -17,23 +17,23 @@ function shiny(atkdef,spespc)
 		return ""
 	end
 end
-
+local partystart
 version = memory.readword(0x14e)
 if version == 0xc1a2 or version == 0x36dc or version == 0xd5dd or version == 0x299c then
 	print("RBGY JPN game detected")
-	base_address = 0xd12b
+	partystart = 0xd12b
 elseif version == 0xe691 or version == 0xa9d then
 	print("red/blue USA detected")
-	base_address = 0xd16b
+	partystart = 0xd16b
 elseif version == 0x7c04 then
 	print("yellow USA detected")
-	base_address = 0xd16a
+	partystart = 0xd16a
 elseif version == 0xd289 or version == 0x9c5e or version == 0xdc5c or version == 0xbc2e or version == 0x4a38 or version == 0xd714 or version == 0xfc7a or version == 0xa456 then
 	print("red/blue EUR detected")
-	base_address = 0xd170
+	partystart = 0xd170
 elseif version == 0x8f4e or version == 0xfb66 or version == 0x3756 or version == 0xc1b7 then
 	print("yellow EUR detected")
-	base_address = 0xd16f
+	partystart = 0xd16f
 else
 	print(string.format("unknown versione, code: %4x", version))
 	print("script stopped")
@@ -41,7 +41,6 @@ else
 end
 
 while true do
-	partystart = 0xd170
 	partysize = memory.readbyte(partystart-8)-1
 	
 	p = 1
