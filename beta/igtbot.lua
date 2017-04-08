ee0aa9d0af6ee3e4e0c690090020e41ad511f49e
LegoFigure11 <legofigurefb@gmail.com>
2017-04-08 21:56:10 +1000

    Bugfix for Spc/Spe separation in output, Caps, etc

--- beta/igtbot.lua
+++ beta/igtbot.lua
@@ -7,19 +7,19 @@ if version == 0xc1a2 or version == 0x36dc or version == 0xd5dd or version == 0x2
 	print("RBGY JPN game detected")
   base_address = 0xd123
 elseif version == 0xe691 or version == 0xa9d then
-	print("red/blue USA detected")
+	print("RB USA detected")
   base_address = 0xd163
 elseif version == 0x7c04 then
-	print("yellow USA detected")
+	print("Yellow USA detected")
   base_address = 0xd162
 elseif version == 0xd289 or version == 0x9c5e or version == 0xdc5c or version == 0xbc2e or version == 0x4a38 or version == 0xd714 or version == 0xfc7a or version == 0xa456 then
-	print("red/blue EUR detected")
+	print("RB EUR detected")
   base_address = 0xd168
 elseif version == 0x8f4e or version == 0xfb66 or version == 0x3756 or version == 0xc1b7 then
-	print("yellow EUR detected")
+	print("Yellow EUR detected")
   base_address = 0xd167
 else
-	print(string.format("unknown versione, code: %4x", version))
+	print(string.format("unknown version, code: %4x", version))
   print("script stopped")
   return
 end
@@ -66,10 +66,10 @@ while true do
 	print(spespc)
     if shiny(atkdef,spespc) then
         print("Shiny!!! Script stopped.")
-        print(string.format("atk: %d", math.floor(atkdef/16)))
-        print(string.format("def: %d", atkdef%16))
-        print(string.format("spe: %d", math.floor(spespc/16)))
-        print(string.format("spe: %d", spespc%16))
+        print(string.format("Atk: %d", math.floor(atkdef/16)))
+        print(string.format("Def: %d", atkdef%16))
+        print(string.format("Spc: %d", math.floor(spespc/16)))
+        print(string.format("Spe: %d", spespc%16))
         savestate.save(state)
         break
     else
