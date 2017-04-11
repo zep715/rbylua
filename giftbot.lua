e23472def4e4972deb5c0fcc77cbde1fbec5e16a
zep715 <zep715@gmail.com>
2017-04-11 13:23:39 +0200

    fix spe/spc print, other prints

--- giftbot.lua
+++ giftbot.lua
@@ -7,20 +7,20 @@ if version == 0xc1a2 or version == 0x36dc or version == 0xd5dd or version == 0x2
 	print("RBGY JPN game detected")
   base_address = 0xd123
 elseif version == 0xe691 or version == 0xa9d then
-	print("RB USA detected")
+	print("Red/Blue USA detected")
   base_address = 0xd163
 elseif version == 0x7c04 then
-	print("Y USA detected")
+	print("Yellow USA detected")
   base_address = 0xd162
 elseif version == 0xd289 or version == 0x9c5e or version == 0xdc5c or version == 0xbc2e or version == 0x4a38 or version == 0xd714 or version == 0xfc7a or version == 0xa456 then
-	print("RB EUR detected")
+	print("Red/Blue EUR detected")
   base_address = 0xd168
 elseif version == 0x8f4e or version == 0xfb66 or version == 0x3756 or version == 0xc1b7 then
-	print("Y EUR detected")
+	print("Yellow EUR detected")
   base_address = 0xd167
 else
-	print(string.format("unknown version, code: %4x", version))
-  print("script stopped")
+	print(string.format("Unknown version, code: %4x", version))
+  print("Script stopped")
   return
 end
 
@@ -57,15 +57,12 @@ while true do
     end
     atkdef = memory.readbyte(dv_addr)
     spespc = memory.readbyte(dv_addr+1)
-    --print(i)
-    print(atkdef)
-    print(spespc)
     if shiny(atkdef,spespc) then
         print("Shiny!!! Script stopped.")
-        print(string.format("atk: %d", math.floor(atkdef/16)))
-        print(string.format("def: %d", atkdef%16))
-        print(string.format("spe: %d", math.floor(spespc/16)))
-        print(string.format("spe: %d", spespc%16))
+        print(string.format("Atk: %d", math.floor(atkdef/16)))
+        print(string.format("Def: %d", atkdef%16))
+        print(string.format("Spe: %d", math.floor(spespc/16)))
+        print(string.format("Spc: %d", spespc%16))
         savestate.save(state)
         break
     else
